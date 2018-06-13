5472cedc336e3ef77c720f653c0092489e0af48f
Tommy Marshment-Howell <tommy.marshmenthowell@wisconsinhistory.org>
2018-06-13 14:46:23 -0500

    Set default frontmatter format to YAML.

--- Marker/Markdown.cs
+++ Marker/Markdown.cs
@@ -5,7 +5,7 @@ namespace Marker
 {
     public static class Markdown
     {
-        public static Frontmatter.Formats FrontmatterFormat { get; set; }
+        public static Frontmatter.Formats FrontmatterFormat { get; set; }  = Frontmatter.Formats.YAML;
         public static string FrontmatterDelimiter { get; set; } = "------";
 
         public static T Deserialize<T>(Stream stream) {
